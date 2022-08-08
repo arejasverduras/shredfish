9c66c23c10807f3372b3b0fc9212118279fabf16
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-08-08 19:01:09 +0200

    add windslice to store

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import ForeCastDataSlice from '../features/ForeCastData/ForeCastDataSlice';
 import TidesResultSlice from '../components/TidesResult/TidesResultSlice';
 import SpotSlice from '../components/SpotSelector/SpotSlice';
+import WindSlice from '../components/WindResult/WindSlice';
 
 export const store = configureStore({
   reducer: {
     spot: SpotSlice,
     forecast: ForeCastDataSlice,
-    tides: TidesResultSlice
+    tides: TidesResultSlice,
+    wind: WindSlice
   },
 });
