9976f2069f4185cf1c35ddec73c109058f68e00d
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-08-09 19:19:46 +0200

    make weather slice

---
+++ src/components/WeatherResult/WeatherSlice.js
@@ -0,0 +1,44 @@
+import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+import Surfline from "../../features/ForeCastData/Surfline";
+
+export const getWeatherData = createAsyncThunk('/weather/weatherinfo',
+async (arg, thunkAPI) => {
+    const response = await Surfline.getWeatherData(arg);
+    return response.data;
+})
+
+const weatherSlice = createSlice({
+    name: 'weather',
+    initialState: {
+        weatherStatus: 'idle',
+        weatherData: ''
+    },
+    reducers: {
+
+    },
+    extraReducers: {
+        [getWeatherData.pending]: (state,action) => {
+            state.weatherStatus = 'loading';
+            
+        },
+        [getWeatherData.fulfilled]: (state,action) => {
+            state.weatherStatus = 'succeeded';
+            state.weatherData = (action.payload)
+            // for analyzing of the response object
+            console.log(state.weatherData)
+        },
+        [getWeatherData.rejected]: (state, action) => {
+            state.weatherStatus = 'rejected';
+            state.weatherData = 'The sun rose in the east';
+        }
+    }
+})
+
+// export slice actions
+
+//create and export sliceSelector 
+export const selectWeatherData = state => state.weather.weatherData;
+export const selectWeatherStatus = state => state.weather.weatherStatus;
+
+// export the reducer as default
+export default weatherSlice.reducer;
