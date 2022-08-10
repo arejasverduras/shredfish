45289e59db15bb2e93504a820f2ec520da5b9b42
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-08-10 09:58:31 +0200

    make TidesStatus component

---
+++ src/components/TidesResult/TidesStatus/TidesStatus.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectTidesStatus } from '../TidesResultSlice';
+
+export const TidesStatus = () =>{
+    return (
+        <>
+        </>
+    )
+}
