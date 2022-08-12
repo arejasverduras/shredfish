952ed7a26208edcaf7e11832ea8ada215658a960
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-08-12 20:49:16 +0200

    add search term and setSearchTerm function to spot slice

--- src/components/SpotLoader/SpotLoader.js
+++ src/components/SpotLoader/SpotLoader.js
@@ -1,12 +1,16 @@
 import {React, useEffect} from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setSearchTerm, selectSearchTerm } from "../SpotSelector/SpotSlice";
 
 export const SpotLoader = () => {
     // should : return an input field for a location name
     //should: set the currentSearchTerm state
+    const dispatch = useDispatch();
+    const searchTerm = useSelector(selectSearchTerm)
     
     return (
         <>
-         
+         <input onChange={dispatch(setSearchTerm())} value={searchTerm}></input>
         </>
     )
 }
