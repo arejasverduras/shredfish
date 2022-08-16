0b9e509b14e572c538bcd5e5e60a4ed16d2dcbd9
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-08-16 13:59:15 +0200

    make the api call to https in geocoding

--- src/features/OpenWeather/GeoCoding.js
+++ src/features/OpenWeather/GeoCoding.js
@@ -2,7 +2,7 @@ const GeoCoding = {
     api: 'eaf16b774db5bd5bdbc862aa052204bb',
     async getGeoCords(arg){
         // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-        const baseURL = 'http://api.openweathermap.org/geo/1.0/direct?q=';
+        const baseURL = 'https://api.openweathermap.org/geo/1.0/direct?q=';
         const appId = `&appid=${this.api}`;
         const params = arg;
         const reqURL = baseURL+params+appId;
