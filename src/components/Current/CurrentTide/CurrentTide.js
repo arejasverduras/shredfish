f16c645e634d3b57076cdf82118b76385fcb29a4
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-08-25 12:32:13 +0200

    set responsive styles for current section (CurrentWrapper)

--- src/components/Current/CurrentTide/CurrentTide.js
+++ src/components/Current/CurrentTide/CurrentTide.js
@@ -1,17 +1,13 @@
 import {React} from 'react';
 import { TidesGraphSG } from '../../Tides/TidesGraphSG/TidesGraphSG';
-import { TideType } from './TideType/TideType';
 
 export const CurrentTide = () => {
 
 return (
-    <>
         <TidesGraphSG  
             hourStart={0} 
-            hourEnd={23}
+            hourEnd={24}
             dayNo={0}
         />
-        {/* <TideType /> */}
-    </>
 )
 }
